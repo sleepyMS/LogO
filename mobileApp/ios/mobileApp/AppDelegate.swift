a56a265cb6739422732a2f17a7079be26a402036
sleepyMS <119601168+sleepyMS@users.noreply.github.com>
2025-02-24 01:31:31 +0900

    ios firebase 앱 등록 완료 Test Success

--- mobileApp/ios/mobileApp/AppDelegate.swift
+++ mobileApp/ios/mobileApp/AppDelegate.swift
@@ -3,9 +3,14 @@ import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 
+import FirebaseCore
+
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+    FirebaseApp.configure()
+    
     self.moduleName = "mobileApp"
     self.dependencyProvider = RCTAppDependencyProvider()
 
